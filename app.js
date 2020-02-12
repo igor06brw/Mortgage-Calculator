31a3a11623b1268fc63e3068b2c5c5376924f530
Igor <igor06brw@gmail.com>
2020-02-12 04:16:16 +0100

    Add radio months/years choice

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const sum = document.querySelector('#mortgageSum');
 const percent = document.querySelector('#mortgagePercent');
-const ages = document.querySelector('#mortgageAges');
+const months = document.querySelector('#mortgageMonths');
 const button = document.querySelector('.calculator__button');
+const radioYear = document.querySelector('#yearsChoice');
+const radioMonth = document.querySelector('#monthsChoice');
 const results = document.querySelector('.results');
+
 
 
 button.addEventListener('click', displayResult);
 
 function displayResult() {
-        const   q = 1 + (percent.value / 100) / 12,
-                R = sum.value * Math.pow(q, ages.value).toPrecision(6) * ((q-1) / (Math.pow(q, ages.value)-1).toPrecision(6));
+        
+        const q = 1 + (percent.value / 100) / 12;
+        let R = sum.value * Math.pow(q, months.value).toPrecision(6) * ((q-1) / (Math.pow(q, months.value)-1).toPrecision(6));
 
 
         const text  = document.createElement('h3'),
@@ -18,9 +22,11 @@ function displayResult() {
 
         results.firstChild.remove();
 
-
-        console.log(results.firstChild);
-
+        if(radioYear.checked == true) {
+                const ages = months.value * 12
+                R = sum.value * Math.pow(q, ages).toPrecision(6) * ((q-1) / (Math.pow(q, ages)-1).toPrecision(6));  
+        }
+        
 
         text.classList.add('results__text');
         spanValue.classList.add('results__value');
