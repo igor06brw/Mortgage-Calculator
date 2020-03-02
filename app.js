f53646aeefc4bf62db59afa6fb4a470f41982472
Igor <igor06brw@gmail.com>
2020-03-02 17:13:23 +0100

    Create display error message, start writing validator

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const sum = document.querySelector('#mortgageSum'),
       results = document.querySelector('.results'),
       textFromLabel = document.querySelector('.calculator__label--month'),
       history = document.querySelector('.history');
-
+      error = document.querySelector('.error');
 
 let arrayOfResults = [];
 
@@ -18,10 +18,9 @@ radioYear.addEventListener('click', function() {
 radioMonth.addEventListener('click', function() {
         textFromLabel.innerHTML = "Ilość miesięcy hipoteki";
 })
+button.addEventListener('click', validator);
 
-button.addEventListener('click', displayResult);
-
-function displayResult() {
+function calculate() {
         
         const q = 1 + (percent.value / 100) / 12;
         let R = sum.value * Math.pow(q, months.value).toPrecision(6) * ((q-1) / (Math.pow(q, months.value)-1).toPrecision(6));
@@ -33,6 +32,10 @@ function displayResult() {
 
         arrayOfResults.push(R);
 
+        displayResult(R);
+        historyFromResults();
+}
+function displayResult(result) {
         const text  = document.createElement('h3'),
               spanValue = document.createElement('span'),
               spanCurrency = document.createElement('span');
@@ -43,17 +46,15 @@ function displayResult() {
         spanValue.classList.add('results__value');
         spanCurrency.classList.add('results__currency');
     
+
         text.innerHTML = 'Twoja rata wynosi: ';
-        spanValue.innerHTML = R.toPrecision(6);
+        spanValue.innerHTML = result.toPrecision(6);
         spanCurrency.innerHTML = ' PLN';
     
         text.appendChild(spanValue);
         text.appendChild(spanCurrency);
         results.appendChild(text);
-
-        historyFromResults();
 }
-
 function historyFromResults() {
                 const content = document.createElement('div'),
                       number = document.createElement('p');
@@ -65,6 +66,32 @@ function historyFromResults() {
 
                 content.appendChild(number);
                 history.appendChild(content);
+}
+function validator() {
+        if(sum.value && percent.value && months.value) {
+                calculate()
+        } else {
+                displayError('Brak podanych wartości!');
+        }
+}
+
+function displayError(name) {
+//   error.firstChild.remove();
+        
+  const container = document.createElement('div'),
+      paragraph = document.createElement('p');
+
+  container.classList.add('error__container');
+  paragraph.classList.add('error__paragraph');
+
+  paragraph.innerHTML = name;
+
+  container.appendChild(paragraph);
+  error.appendChild(container); 
+
+  setTimeout(() => {
+        error.firstChild.remove();
+  }, 1000);
 }
 
 
